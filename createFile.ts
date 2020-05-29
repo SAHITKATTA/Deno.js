bf947b0b6633e955e3528539caa48a0a51fae8a4
Katta Sahit Mohan <sahit.k@comakeit.com>
2020-05-29 09:57:32 +0530

    added comments in createFile.ts

--- createFile.ts
+++ createFile.ts
@@ -2,7 +2,7 @@ const encoder = new TextEncoder()
 
 const greetText = encoder.encode('Hello World\nMy name is Sahit')
 
-await Deno.writeFile('greet.txt', greetText);
+await Deno.writeFile('greet.txt', greetText); //await because its a promise
 
 // deno run --allow-write createFile.ts 
 // as we have to grant access for write access
